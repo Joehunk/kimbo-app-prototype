6f44b1418e26650bb3866ddb54f256c130398e63
Charles Crain <charles.crain@kuka.com>
2018-07-07 16:34:19 -0500

    Some progress on ingredient list

--- app.js
+++ app.js
@@ -19,6 +19,7 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const vision = require('@google-cloud/vision');
+const flatMap = require('flatmap');
 
 const app = express();
 
@@ -26,6 +27,79 @@ const client = new vision.ImageAnnotatorClient();
 
 app.use(fileUpload());
 app.use(express.static('www'));
+
+function getParagraphsFromTextAnnotation(fullTextAnnotation) {
+    return flatMap(fullTextAnnotation.pages, page => {
+        return flatMap(page.blocks, block => {
+            return block.paragraphs.map(paragraph => {
+                const wordsInParagraph = paragraph.words.map(word => {
+                    return word.symbols.map(s => s.text).join("")
+                });
+                console.log("Paragraph: " + wordsInParagraph.join(" "));
+                return wordsInParagraph.join(" ")
+            });
+        })
+    });
+}
+
+function isConsistentCase(text) {
+    return text === text.toLowerCase() || text === text.toUpperCase()
+}
+
+function hasWordsUncommonToIngredients(ingredient) {
+    const articleRegex = /(^|[^a-z])(a|and|the|or|by)($|[^a-z])/i;
+
+    return articleRegex.test(ingredient)
+}
+
+function wordCount(text) {
+    return text.split(/\s+/).length
+}
+
+function isPossibleIngredient(ingredient) {
+    const containsLetters = /[A-Z]/i
+
+    return containsLetters.test(ingredient) && wordCount(ingredient) <= 4
+}
+
+function extractIngredientsFromText(text) {
+    const ingredientsRegex = /.*(ingredients|made\s+of)/i
+
+    if (ingredientsRegex.test(text)) {
+        const ingredientListSubstring = text.replace(ingredientsRegex, "");
+
+        return ingredientListSubstring.split(/\s*[.,:;()]\s*/).filter(isPossibleIngredient).map(s => s.toLowerCase())
+    } else {
+        return []
+    }
+}
+
+function extractIngredientsFromTextAnnotation(textAnnotation) {
+    const paragraphs = getParagraphsFromTextAnnotation(textAnnotation);
+
+    return flatMap(paragraphs, extractIngredientsFromText)
+
+    // const lines = textAnnotation.text.split("\n");
+    // var ingredientsBegun = false;
+    //
+    // const ingredientStringFragments = flatMap(lines, line => {
+    //     if (!ingredientsBegun) {
+    //         if (isFirstLineOfIngredients(line)) {
+    //             ingredientsBegun = true
+    //         }
+    //     }
+    // })
+}
+
+function renderIngredientsToHtmlFromTextAnnotation(textAnnotation) {
+    const ingredients = extractIngredientsFromTextAnnotation(textAnnotation);
+
+    return `<b>INGREDIENTS</b><br>
+<ul>
+${ingredients.map(i => "<li>" + i).join("\n")}
+</ul>
+`
+}
 
 app.post('/upload_ingredients', function(req, res) {
     if (!req.files)
@@ -36,16 +110,18 @@ app.post('/upload_ingredients', function(req, res) {
     const request = {
         image: {
             content: ingredients.data
+        },
+        imageContext: {
+            languageHints: [ "en" ]
         }
     };
     client
-        .textDetection(request)
+        .documentTextDetection(request)
         .then(results => {
-            const labels = results[0].textAnnotations;
+            const textAnnotation = results[0].fullTextAnnotation;
 
-            console.log('Labels:');
-            labels.forEach(label => console.log(label.description));
-            res.status(200).send(labels.map(l => l.description).join(" -- "))
+            console.log(textAnnotation.text);
+            res.status(200).send(renderIngredientsToHtmlFromTextAnnotation(textAnnotation))
         })
         .catch(err => {
             console.error('ERROR:', err);
