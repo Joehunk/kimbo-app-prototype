c2d627b11555d9ad779f16512e0b40dd7352cb24
Charles Crain <charles.crain@kuka.com>
2018-07-07 19:00:40 -0500

    Added bootstrap for better look

--- app.js
+++ app.js
@@ -25,8 +25,27 @@ const app = express();
 
 const client = new vision.ImageAnnotatorClient();
 
+const header = `    
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+`;
+
+const scripts = `<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+`;
+
 app.use(fileUpload());
 app.use(express.static('www'));
+app.use(function (err, req, res, next) {
+    res.status(500).send(simpleHtml("An error occurred."))
+});
 
 function getParagraphsFromTextAnnotation(fullTextAnnotation) {
     return flatMap(fullTextAnnotation.pages, page => {
@@ -94,16 +113,38 @@ function extractIngredientsFromTextAnnotation(textAnnotation) {
 function renderIngredientsToHtmlFromTextAnnotation(textAnnotation) {
     const ingredients = extractIngredientsFromTextAnnotation(textAnnotation);
 
-    return `<b>INGREDIENTS</b><br>
+    return renderHtml(`
+<b>INGREDIENTS</b><br>
 <ul>
 ${ingredients.map(i => "<li>" + i).join("\n")}
 </ul>
+`)
+}
+
+function renderHtml(bodyHtml) {
+    return `
+<html>
+<head>
+${header}
+<title>Kimbo Prototype App</title>
+</head>
+<body>
+${bodyHtml}
+<p><a href="/">Try again...</a></p>
+${scripts}
+</body>
+</html>
 `
 }
 
+function simpleHtml(message) {
+    return renderHtml(`<p>${message}</p>`)
+}
+
 app.post('/upload_ingredients', function(req, res) {
-    if (!req.files)
-        return res.status(400).send('No files were uploaded.');
+    if (!req.files || !req.files.ingredients) {
+        return res.status(400).send(simpleHtml("No files were uploaded."));
+    }
 
     let ingredients = req.files.ingredients;
 
@@ -123,9 +164,6 @@ app.post('/upload_ingredients', function(req, res) {
             console.log(textAnnotation.text);
             res.status(200).send(renderIngredientsToHtmlFromTextAnnotation(textAnnotation))
         })
-        .catch(err => {
-            console.error('ERROR:', err);
-        });
 });
 
 // Start the server
